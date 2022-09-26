57f51a901f4fa3095b5aab28ae9e79d114fa76f0
xartola <xabiarzab@gmail.com>
2022-09-26 18:10:53 +0200

    Aldaketak testeatzeko egituran

--- src/test/java/DiruaSartuDAWTest.java
+++ src/test/java/DiruaSartuDAWTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 import java.util.Date;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -18,21 +19,19 @@ import domain.Blokeoa;
 import domain.Erabiltzailea;
 import domain.Pertsona;
 import exceptions.MezuaEzDaZuzena;
-
+import test.dataAccess.TestDataAccess;
 
 public class DiruaSartuDAWTest {
 
-	static DataAccess dbManager;
-
-	static Erabiltzailea er1;
-
-	static Erabiltzailea er2;
-
-	static Erabiltzailea er3;
-
-	static Erabiltzailea er4;
+	static Erabiltzailea er1, er2, er3, er4;
 	static Admin ad1;
 	static Blokeoa bl1;
+
+	// sut:system under test
+	static DataAccess dbManager;
+
+	// additional operations needed to execute the test
+	static TestDataAccess testDA;
 
 	@BeforeClass
 	public static void initializeDB() {
@@ -44,50 +43,55 @@ public class DiruaSartuDAWTest {
 			dbManager.initializeDB();
 		} else
 			dbManager = new DataAccess();
+
+		testDA = new TestDataAccess();
+
+		// Erabiltzaile honek ez erregitratua egon behar du
+		er1 = new Erabiltzailea();
+		er1.setIzena("erab1");
 		
 		// Erabiltzaileak sortu
-		
-				// Erabiltzaile honek ez erregitratua egon behar 
-				er1 = new  Erabiltzailea();
-				er1.setIzena("erab1");
-			
-				er2 = (Erabiltzailea) dbManager.erregistratu("erab2", "1234", new Date(2000, 01, 01));
-				er3 = (Erabiltzailea) dbManager.erregistratu("erab3", "1234", new Date(2000, 01, 01));
-				er4 = (Erabiltzailea) dbManager.erregistratu("erab4", "1234", new Date(2000, 01, 01));
-			
-				Admin ad1 = new Admin("admin2", "pass", new Date());
+		er2 = (Erabiltzailea) dbManager.erregistratu("erab2", "1234", new Date(2000, 01, 01));
+		er3 = (Erabiltzailea) dbManager.erregistratu("erab3", "1234", new Date(2000, 01, 01));
+		er4 = (Erabiltzailea) dbManager.erregistratu("erab4", "1234", new Date(2000, 01, 01));
 
-				try {
-					dbManager.erabiltzaileaBlokeatu(ad1, er3, null);
-				} catch (MezuaEzDaZuzena e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			
+		Admin ad1 = (Admin) dbManager.getErabiltzailea("admin");
+
+		try {
+			dbManager.erabiltzaileaBlokeatu(ad1, er3, null);
+		} catch (MezuaEzDaZuzena e) {
+			e.printStackTrace();
+		}
+
 		dbManager.close();
 	}
-	
-	
+
 	@Before
 	public void open() {
 		dbManager.open(false);
 	}
 
-
 	@After
 	public void close() {
-		dbManager.close();
+		
 	}
+	
+	@AfterClass
+	public static void ezabatu() {
+		
+		assertTrue(testDA.removePertsona("erab2"));
+		assertTrue(testDA.removePertsona("erab3"));
+		assertTrue(testDA.removePertsona("erab4"));	
+	}
+	
 
 	@Test
 	public void diruaSarrtuTest1() {
-		
+
 		boolean expected = false;
 		boolean actual = dbManager.diruaSartu(er1, "1234", 2.0);
 		assertEquals(expected, actual);
-	
-		
+
 	}
 
 	@Test
@@ -109,12 +113,14 @@ public class DiruaSartuDAWTest {
 
 	@Test
 	public void diruaSartuTest4() {
-		
+
 		boolean expected = true;
 		boolean actual = dbManager.diruaSartu(er4, "1234", 22.0);
 
 		assertEquals(expected, actual);
-	
+		
+		
+
 	}
 
 }
