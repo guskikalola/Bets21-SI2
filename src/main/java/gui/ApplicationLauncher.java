5f47722b120808006de23d4a33bd4311916116bb
aizpu <aizpu@DESKTOP-VSB37HC>
2022-11-05 16:06:10 +0100

    BusinessFactory sortuta

--- src/main/java/gui/ApplicationLauncher.java
+++ src/main/java/gui/ApplicationLauncher.java
@@ -12,6 +12,7 @@ import configuration.ConfigXML;
 import dataAccess.DataAccess;
 import businessLogic.BLFacade;
 import businessLogic.BLFacadeImplementation;
+import businessLogic.BusinessFactory;
 
 public class ApplicationLauncher {
 
@@ -28,47 +29,17 @@ public class ApplicationLauncher {
 		MainGUI a = new MainGUI();
 		a.setVisible(true);
 
+		BusinessFactory bf = null;
 	
 		// LoginGUI lG = new LoginGUI();
 		// lG.setVisible(true);
 
 		try {
-
-			BLFacade appFacadeInterface;
+			BLFacade appFacadeInterface = null;
 //			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
 //			UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
 			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-
-			if (c.isBusinessLogicLocal()) {
-
-				// In this option the DataAccess is created by FacadeImplementationWS
-				// appFacadeInterface=new BLFacadeImplementation();
-
-				// In this option, you can parameterize the DataAccess (e.g. a Mock DataAccess
-				// object)
-
-				DataAccess da = new DataAccess(c.getDataBaseOpenMode().equals("initialize"));
-				appFacadeInterface = new BLFacadeImplementation(da);
-
-			}
-
-			else { // If remote
-
-				String serviceName = "http://" + c.getBusinessLogicNode() + ":" + c.getBusinessLogicPort() + "/ws/"
-						+ c.getBusinessLogicName() + "?wsdl";
-
-				// URL url = new URL("http://localhost:9999/ws/ruralHouses?wsdl");
-				URL url = new URL(serviceName);
-
-				// 1st argument refers to wsdl document above
-				// 2nd argument is service name, refer to wsdl document above
-//		        QName qname = new QName("http://businessLogic/", "FacadeImplementationWSService");
-				QName qname = new QName("http://businessLogic/", "BLFacadeImplementationService");
-
-				Service service = Service.create(url, qname);
-
-				appFacadeInterface = service.getPort(BLFacade.class);
-			}
+			appFacadeInterface = bf.createBusiness(c, appFacadeInterface);
 			/*
 			 * if (c.getDataBaseOpenMode().equals("initialize"))
 			 * appFacadeInterface.initializeBD();
